fb83cde408fab9f0bac5503000e136f44e125fa4
programadorisgod <catapiascastro@gmail.com>
2026-02-06 17:09:05 -0500

    Remove InsufficientBalanceError class

--- src/utils/error.ts
+++
@@ -1,6 +0,0 @@
-export class InsufficientBalanceError extends Error {
-  constructor(provider: string) {
-    super(`${provider}: insufficient balance`);
-    this.name = "InsufficientBalanceError";
-  }
-}
